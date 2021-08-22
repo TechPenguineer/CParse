97f5541bd4c5bbd51f57bc4c7a73c321b79633fb
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-21 20:34:23 -0400

    create parser definition

---
+++ CParser.c
@@ -0,0 +1,14 @@
+#include "cparser.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void Parse(char* STRING, char* SEPERATOR)
+{
+    int LENGTH = strlen(STRING);
+    int INDEX;
+    for(INDEX=0;INDEX<LENGTH;INDEX++)
+    {
+        
+    }
+}
---
+++ cparser.h
@@ -0,0 +1,5 @@
+#ifndef CPARSER_H
+#define CPARSER_H
+
+void Parse(char* STRING, char* SEPERATOR);
+#endif
